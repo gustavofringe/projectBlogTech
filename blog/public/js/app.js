e074be4fa5e1a38fbbb2a38c9b43c723e70d5d1b
Dussart Guillaume <dussartguillaume@orange.fr>
2017-06-15 12:10:05 +0200

    add images

--- blog/public/js/app.js
+++ blog/public/js/app.js
@@ -1,3 +1,16 @@
-$(document).ready(function () {
-    alert('helloworld')
-});
+var img = document.getElementsByTagName("img");
+var text = document.getElementById("text");
+var title = document.querySelector(".title")
+
+for (var i = 0; i < img.length; i++) {
+    img[i].addEventListener("mouseover", function () {
+        var alt = this.alt;
+        text.textContent = alt;
+        var x = this.title
+        title.textContent = x
+    });
+    img[i].addEventListener('mouseout', function () {
+        text.textContent = ''
+        title.textContent = ''
+    })
+}
