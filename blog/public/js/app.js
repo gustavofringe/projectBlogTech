d408274598f3b3e1b3ea85b93eb45e63d397cb90
Dussart Guillaume <dussartguillaume@orange.fr>
2017-06-16 16:47:31 +0200

    add js

--- blog/public/js/app.js
+++ blog/public/js/app.js
@@ -1,19 +1,64 @@
-var img = document.getElementsByTagName("img");
-var text = document.getElementById("text");
-var title = document.querySelector(".title")
+/*
+ *
+ * alt
+ *
+ * */
+
+var img = document.getElementsByTagName("img")
+var text = document.getElementById("text")
 
 for (var i = 0; i < img.length; i++) {
     img[i].addEventListener("mouseover", function () {
-        var alt = this.alt;
-        text.textContent = alt;
-        var x = this.title
-        title.textContent = x
+        var alt = this.alt
+        text.textContent = alt
     });
     img[i].addEventListener('mouseout', function () {
         text.textContent = ''
-        title.textContent = ''
     })
 }
-var d = new Date()
-var ds = d.getSeconds(1000)
-document.write(ds).setTimeout((0,1000))
+/*
+ *
+ * slide
+ *
+ *
+ * */
+var index = 0;
+
+function slide() {
+    var x = document.getElementsByClassName("slide");
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    index++;
+    if (index > x.length) {
+        index = 1
+    }
+    x[index - 1].style.display = "block";
+    setTimeout(slide, 3000);
+}
+slide();
+/**
+ *
+ *
+ *tabs
+ *
+ * */
+var tab = document.querySelector('.list')
+var texte = document.querySelector('.lorem')
+for (var i = 0; i < tab.length; i++) {
+    tab.addEventListener('click', function () {
+
+        texte.classList.toggle('active')
+
+    });
+}
+
+/**
+ * *
+ * *
+ * *date
+ * *
+ * */
+var a = new Date().toLocaleDateString();
+document.querySelector('.date').innerHTML = a
+
