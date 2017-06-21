1b2315bef286f8be01cab91b6f25d9e41718518e
Dussart Guillaume <dussartguillaume@orange.fr>
2017-06-21 15:40:30 +0200

    add content in table

--- wp-content/themes/newstech/index.php
+++ wp-content/themes/newstech/index.php
@@ -18,7 +18,6 @@
         <div class="d-block">
                     <?php
                     while (have_posts()):the_post();
-// Si l'article n'a pas d'Image à la Une, on récupère la 1ère image attachée
                             $attachments = get_children(
                                 array(
                                     'post_parent' => get_the_ID(),
@@ -44,15 +43,12 @@
                         <div class="post-user">
                             <i class="icon-user d-inline"></i>
                             <span class="user d-inline"><?php the_author(); ?></span>
-                            <span class="post_date d-inline"><?php the_time('D j M Y H:i');?></span>
+                            <span class="post_date d-inline"><?php the_time('j M Y');?></span>
                             <i class="icon-comment-alt d-inline"></i>
-                            <span class="d-inline"><a href="template-parts/show.html"><?php comments_number(); ?></a></span>
+                            <span class="d-inline"><a href="<?php the_permalink(); ?>"><?php comments_number(); ?></a></span>
                         </div>
 
-                        <p class="mt-3"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet
-                            aperiam, beatae cum earum enim
-                            et eum facere iusto maxime nesciunt nobis provident quia quos ratione sed similique suscipit
-                            tempora....
+                        <p class="mt-3">  <?php the_excerpt(); ?>
                         </p>
                         <div class="btn btn-secondary btn-lg btn-block"><a href="<?php the_permalink(); ?>">Lire la
                                 suite >></a></div>
@@ -61,9 +57,7 @@
 
             </div>
 
-                <div class="pub mt-4 hidden-md-down">
-                    <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/pub.jpg" alt="publicité">
-                </div>
+
                                     <?php
                                 endforeach;
 
@@ -74,89 +68,10 @@
                         <img src="<?php /*echo esc_url(get_template_directory_uri()); */?>/img/img_blog1.jpg"
                              alt="enceinte bluetooth">
                     </div>-->
-
-            <div class="new m-4">
-                <div class="row pb-4">
-                    <div class="col-md-12 col-lg-6">
-                        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/img_blog2.png"
-                             alt="enceinte bluetooth">
-                    </div>
-                    <div class="col-md-12 col-lg-6">
-                        <h3>Energy</h3>
-                        <h4>Music Box 5 – La boite à son bluetooth au bon rapport qualité/prix</h4>
-                        <div class="post-user">
-                            <i class="icon-user d-inline"></i>
-                            <span class="user d-inline">Fabien Pinel</span>
-                            <span class="post_date d-inline">le 15 mai 2017</span>
-                            <i class="icon-comment-alt d-inline"></i>
-                            <span class="d-inline"><a href="template-parts/show.html">1 Comment</a></span>
-                        </div>
-
-                        <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet
-                            aperiam, beatae cum earum enim
-                            et eum facere iusto maxime nesciunt nobis provident quia quos ratione sed similique suscipit
-                            tempora....
-                        </p>
-                        <div class="btn btn-secondary btn-lg btn-block"><a href="template-parts/show.html">Lire la
-                                suite >></a></div>
-                    </div>
-                </div>
+            <div class="pub mt-4 hidden-md-down">
+                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/pub.jpg" alt="publicité">
             </div>
-            <div class="new m-4">
-                <div class="row pb-4">
-                    <div class="col-md-12 col-lg-6">
-                        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/img_blog3.jpg"
-                             alt="enceinte bluetooth">
-                    </div>
-                    <div class="col-md-12 col-lg-6">
-                        <h3>Energy</h3>
-                        <h4>Music Box 5 – La boite à son bluetooth au bon rapport qualité/prix</h4>
-                        <div class="post-user">
-                            <i class="icon-user d-inline"></i>
-                            <span class="user d-inline">Fabien Pinel</span>
-                            <span class="post_date d-inline">le 15 mai 2017</span>
-                            <i class="icon-comment-alt d-inline"></i>
-                            <span class="d-inline"><a href="template-parts/show.html">1 Comment</a></span>
-                        </div>
 
-                        <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet
-                            aperiam, beatae cum earum enim
-                            et eum facere iusto maxime nesciunt nobis provident quia quos ratione sed similique suscipit
-                            tempora....
-                        </p>
-                        <div class="btn btn-secondary btn-lg btn-block"><a href="template-parts/show.html">Lire la
-                                suite >></a></div>
-                    </div>
-                </div>
-            </div>
-            <div class="new m-4">
-                <div class="row pb-4">
-                    <div class="col-md-12 col-lg-6">
-                        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/img_blog4.jpg"
-                             alt="enceinte bluetooth">
-                    </div>
-                    <div class="col-md-12 col-lg-6">
-                        <h3>Energy</h3>
-                        <h4>Music Box 5 – La boite à son bluetooth au bon rapport qualité/prix</h4>
-                        <div class="post-user">
-                            <i class="icon-user d-inline"></i>
-                            <span class="user d-inline">Fabien Pinel</span>
-                            <span class="post_date d-inline">le 15 mai 2017</span>
-                            <i class="icon-comment-alt d-inline"></i>
-                            <span class="d-inline"><a href="template-parts/show.html">1 Comment</a></span>
-                        </div>
-
-                        <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet
-                            aperiam, beatae cum earum enim
-                            et eum facere iusto maxime nesciunt nobis provident quia quos ratione sed similique suscipit
-                            tempora....
-                        </p>
-                        <div class="btn btn-secondary btn-lg btn-block"><a href="template-parts/show.html">Lire la
-                                suite >></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
 
     </main>
