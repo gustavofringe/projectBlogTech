2b8e643d0b49e6d5fdf0072a5f6dca6ce7c5efc4
Dussart Guillaume <dussartguillaume@orange.fr>
2017-06-21 15:51:25 +0200

    single.php->change include

--- wp-content/themes/newstech/single.php
+++ wp-content/themes/newstech/single.php
@@ -1,13 +1,7 @@
 <?php
-/**
- * The template for displaying all single posts and attachments
- *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
- */
 
-get_header(); ?>
+
+include "header.php"; ?>
 
 <div id="primary" class="content-area">
     <main id="main" class="container" role="main">
@@ -46,4 +40,4 @@ get_header(); ?>
     </main><!-- .site-main -->
 </div><!-- .content-area -->
 
-<?php get_footer(); ?>
+<?php include "footer.php" ?>
